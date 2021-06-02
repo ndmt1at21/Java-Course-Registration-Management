a6321e64532004259581ed433ffbdc03f9d0c354
ndmt1at21 <trminh36@gmail.com>
2021-06-02 15:10:19 +0700

    refactor db factory

--- src/main/java/com/utils/DBFactory.java
+++ src/main/java/com/utils/DBFactory.java
@@ -1,43 +1,82 @@
 package com.utils;
 
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 public class DBFactory {
-    static <T> void create(T data) {
-        runTransaction(session -> session.save(data));
-    }
-
-    static <T> void update(T data) {
-        runTransaction(session -> session.update(data));
-    }
-
-    static <T> void delete(T data) {
-        runTransaction(session -> session.delete(data));
-    }
-
-    static <T> void deleteByID(Class<T> modelClass, int id) {
-        runTransaction(session -> {
-            session.get(modelClass, id);
+    public static <T> void create(T data) {
+        runTransaction((Session session) -> {
+            session.save(data);
+            return null;
         });
     }
 
-    static <T> void deleteByID(Class<T> modelClass, String id) {
-        runTransaction(session -> {
-            session.get(modelClass, id);
+    public static <T> void update(T data) {
+        runTransaction((Session session) -> {
+            session.update(data);
+            return null;
         });
     }
 
-    private static void runTransaction(CommandRunInTransation actionFn) {
+    public static <T> void delete(T data) {
+        runTransaction((Session session) -> {
+            session.delete(data);
+            return null;
+        });
+    }
+
+    public static <T> void deleteByID(Class<T> modelClass, int id) {
+        runTransaction((Session session) -> {
+            T obj = session.get(modelClass, id);
+
+            if (obj == null)
+                return null;
+            session.delete(obj);
+            return null;
+        });
+    }
+
+    public static <T> void deleteByID(Class<T> modelClass, String id) {
+        runTransaction(session -> {
+            T obj = session.get(modelClass, id);
+
+            if (obj == null)
+                return null;
+            session.delete(obj);
+            return null;
+        });
+    }
+
+    public static <T> Long countAll(String modelClassName) {
+        String queryStr = "select count(*) from " + modelClassName;
+
+        return runTransaction((Session session) -> {
+            Query<Long> query = session.createQuery(queryStr, Long.class);
+            Long count = query.uniqueResult();
+            return count;
+        });
+    }
+
+    /**
+     * 
+     * @param <T>      type return
+     * @param actionFn action function in transaction
+     * @return result when query to database
+     */
+    public static <T> T runTransaction(CommandRunInTransation<T> actionFn) {
         Session session = HibernateUtils.getSessionFactory().openSession();
         session.beginTransaction();
 
-        actionFn.runWith(session);
+        T result = actionFn.runWith(session);
 
         session.getTransaction().commit();
         session.close();
+
+        return result;
     }
 }
 
-interface CommandRunInTransation {
-    public void runWith(Session session);
+@FunctionalInterface
+interface CommandRunInTransation<T> {
+    public T runWith(Session session);
 }
